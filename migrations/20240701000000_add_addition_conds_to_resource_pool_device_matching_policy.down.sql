bc698c23f85fb8bf70b30265d8bee9403f27a4ce
ny <heningyu21@gmail.com>
2025-05-22 21:21:17 +0800

    feat: 更新Navy-NG项目规则集，重构项目结构索引，添加各子规则集的链接和描述，简化项目文档结构，提升可读性和导航性。

--- migrations/20240701000000_add_addition_conds_to_resource_pool_device_matching_policy.down.sql
+++
@@ -1,2 +0,0 @@
--- 删除额外动态条件字段
-ALTER TABLE resource_pool_device_matching_policy DROP COLUMN addition_conds;
--- migrations/20240701000000_add_addition_conds_to_resource_pool_device_matching_policy.up.sql
+++
@@ -1,2 +0,0 @@
--- 添加额外动态条件字段到资源池设备匹配策略表
-ALTER TABLE resource_pool_device_matching_policy ADD COLUMN addition_conds TEXT DEFAULT NULL COMMENT '额外动态条件，JSON格式存储';
